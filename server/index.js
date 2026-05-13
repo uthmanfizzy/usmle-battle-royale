d214194b3a4ea9434ef24256a7a6331c884731da
Dr-Fiz <drfaizaan0@gmail.com>
2026-05-13 13:04:00 +0100

    force cors headers on all responses

--- server/index.js
+++ server/index.js
@@ -34,6 +34,18 @@ const SERVER_URL = process.env.SERVER_URL || `http://localhost:${PORT}`;
 
 const app = express();
 
+// Manual CORS headers - MUST be first middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-admin-password, x-admin-key');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
+
 // CORS configuration - allow all origins
 const corsOptions = {
   origin: function(origin, callback) {
